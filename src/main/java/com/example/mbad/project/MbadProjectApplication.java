d593e25d9509f2456e4b3a9b54145c5979bbefc9
piatek5 <bordowyrys1@gmail.com>
2026-01-13 21:12:51 +0100

    inital commit

---
+++ src/main/java/com/example/mbad/project/MbadProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.mbad.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbadProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbadProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mbad/project/MbadProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mbad.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbadProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
